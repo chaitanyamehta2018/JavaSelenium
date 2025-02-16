3983f8b068c69bda11b0a957c689944e6ddd47dd
chaitanyamehta2018 <chaitanyamehta2018@gmail.com>
2025-02-16 20:58:10 +0530

    Created VerifyPageTitleTest

---
+++ src/test/java/VerifyPageTitleTest.java
@@ -0,0 +1,65 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+public class VerifyPageTitleTest {
+    
+    public VerifyPageTitleTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class JavaSelenium.
+     */
+    @Test
+    public void testMain() throws InterruptedException 
+    {
+        
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-blink-features=AutomationControlled");
+        
+        WebDriver driver = new ChromeDriver(options);
+        
+        driver.get("http://www.google.com");
+        
+        driver.manage().window().maximize();
+        
+        WebElement element = driver.findElement(By.name("q"));
+        
+        element.sendKeys("Sky Force Movie Trailer");
+        
+        var title = driver.getTitle();
+        
+        assertEquals("Google",title, "Title should be Google");
+        
+        Thread.sleep(5000);
+                
+        driver.quit();
+        
+    }
+    
+}
