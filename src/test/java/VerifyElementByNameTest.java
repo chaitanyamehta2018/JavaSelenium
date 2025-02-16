53c9c63aca59415c7ce2424d8fb4e38a638aae49
chaitanyamehta2018 <chaitanyamehta2018@gmail.com>
2025-02-16 22:06:16 +0530

    Created  VerifyElementByNameTest

---
+++ src/test/java/VerifyElementByNameTest.java
@@ -0,0 +1,45 @@
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class VerifyElementByNameTest {
+    
+    public VerifyElementByNameTest(){
+        
+    }
+    
+    @Test
+    public void testMain() throws InterruptedException {
+        WebDriver driver = null;
+        
+        try {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--disable-blink-features=AutomationControlled");
+
+            driver = new ChromeDriver(options);
+            driver.get("https://practicetestautomation.com/practice-test-login/");
+            driver.manage().window().maximize();
+
+            WebElement element1 = driver.findElement(By.name("username"));
+            assertTrue(element1.isDisplayed(), "Username element should be visible");
+  
+            WebElement element2 = driver.findElement(By.name("password"));
+            assertTrue(element2.isDisplayed(), "Password element should be visible");
+                               
+            
+            Thread.sleep(5000);
+        } 
+        finally {
+            if (driver != null) {
+                driver.quit();
+            }
+        }
+        
+    }
+    
+}
