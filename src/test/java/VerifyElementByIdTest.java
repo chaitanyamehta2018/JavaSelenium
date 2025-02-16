9d4abe035f1fed0ba2e39ddd06568c49353b3bf5
chaitanyamehta2018 <chaitanyamehta2018@gmail.com>
2025-02-16 21:56:11 +0530

    Created VerifyElementByIdTest

---
+++ src/test/java/VerifyElementByIdTest.java
@@ -0,0 +1,50 @@
+import java.time.Duration;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+public class VerifyElementByIdTest {
+    
+    public VerifyElementByIdTest(){
+        
+    }
+    
+    @Test
+    public void testMain() throws InterruptedException {
+        WebDriver driver = null;
+        
+        try {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--disable-blink-features=AutomationControlled");
+
+            driver = new ChromeDriver(options);
+            driver.get("https://practicetestautomation.com/practice-test-login/");
+            driver.manage().window().maximize();
+
+            WebElement element1 = driver.findElement(By.id("username"));
+            assertTrue(element1.isDisplayed(), "Username element should be visible");
+  
+            WebElement element2 = driver.findElement(By.id("password"));
+            assertTrue(element2.isDisplayed(), "Password element should be visible");
+                               
+            
+            Thread.sleep(5000);
+        } 
+        finally {
+            if (driver != null) {
+                driver.quit();
+            }
+        }
+        
+    }
+    
+}
