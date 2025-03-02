3c8b55c551540813b4a7b736c17c4eedcefa4ed5
chaitanyamehta2018 <chaitanyamehta2018@gmail.com>
2025-03-02 23:19:52 +0530

    Refractored GettingLinkDataAttributesTest

--- src/test/java/Zhimin_Zhan_Exercise/Chapter3_Hyperlink/GettingLinkDataAttributesTest.java
+++ src/test/java/Zhimin_Zhan_Exercise/Chapter3_Hyperlink/GettingLinkDataAttributesTest.java
@@ -1,10 +1,8 @@
 package Zhimin_Zhan_Exercise.Chapter3_Hyperlink;
 
-import XPath.*;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
