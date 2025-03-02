ad04c9cd6b501eef3d0ac8fe0b61fa359e61dedd
chaitanyamehta2018 <chaitanyamehta2018@gmail.com>
2025-03-02 22:18:59 +0530

    Created ClickLinkByTextTest.html

---
+++ src/test/java/Zhimin_Zhan_Exercise/ClickLinkByTextTest.java
@@ -0,0 +1,41 @@
+package Zhimin_Zhan_Exercise;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class ClickLinkByTextTest {
+
+    public ClickLinkByTextTest()
+    {
+    }
+        
+    @Test
+    public void testMain() throws InterruptedException {
+        WebDriver driver = null;
+        
+        try {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--disable-blink-features=AutomationControlled");
+
+            driver = new ChromeDriver(options);
+            driver.get("file:///E:/Softwares/Java/JavaSelenium/src/test/java/Zhimin_Zhan_Exercise/SampleHTMLs/ClickLinkByIDTest.html");
+            driver.manage().window().maximize();
+
+            WebElement element1 = driver.findElement(By.linkText("Go to Example"));
+            element1.click();
+            
+            Thread.sleep(5000);
+        } 
+        finally {
+            if (driver != null) {
+                driver.quit();
+            }
+        }
+        
+    }
+    
+}
