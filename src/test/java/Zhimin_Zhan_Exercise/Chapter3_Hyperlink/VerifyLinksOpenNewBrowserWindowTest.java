1c7efd15cf2c5c5ce6d86a4d3ac103731583cf36
chaitanyamehta2018 <chaitanyamehta2018@gmail.com>
2025-03-08 09:40:20 +0530

    Minor changes

--- src/test/java/Zhimin_Zhan_Exercise/Chapter3_Hyperlink/VerifyLinksOpenNewBrowserWindowTest.java
+++ src/test/java/Zhimin_Zhan_Exercise/Chapter3_Hyperlink/VerifyLinksOpenNewBrowserWindowTest.java
@@ -39,7 +39,7 @@ public class VerifyLinksOpenNewBrowserWindowTest {
             // Validate we are back on the original page
             assertEquals(currentUrl, driver.getCurrentUrl(), "Should return to original page");
 
-            Thread.sleep(4000);
+            Thread.sleep(6000);
         } 
         finally {
             if (driver != null) {
