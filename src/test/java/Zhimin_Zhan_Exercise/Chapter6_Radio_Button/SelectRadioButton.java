6c5db61403a10a0b42e239adb17c1eb5bf462848
chaitanyamehta2018 <chaitanyamehta2018@gmail.com>
2025-03-23 18:20:21 +0530

    Updated SelectRadioButton

--- src/test/java/Zhimin_Zhan_Exercise/Chapter6_Radio_Button/SelectRadioButton.java
+++ src/test/java/Zhimin_Zhan_Exercise/Chapter6_Radio_Button/SelectRadioButton.java
@@ -2,7 +2,7 @@
 package Zhimin_Zhan_Exercise.Chapter6_Radio_Button;
 
 import java.io.File;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -35,17 +35,15 @@ public class SelectRadioButton {
             
             //Finding Elements
              WebElement radioButton = driver.findElement(By.id("java"));
+             
+             Thread.sleep(2000);
+             assertFalse(radioButton.isSelected(), "Radiobutton should NOT be enabled");
+             
              radioButton.click();
              
-             Thread.sleep(6000);
-             radioButton.clear();
-             //String multilineText = "This is line 1\nThis is line 2\nThis is line 3";
+             Thread.sleep(2000);
              
-             //textArea.sendKeys(multilineText);
-             //submitButton.click();
-             
-             // String displayedText = driver.findElement(By.id("output")).getText();
-              //assertEquals(multilineText, displayedText.trim(), "Output text should match input text");            
+              assertTrue(radioButton.isSelected(), "Radiobutton should be enabled");            
             
             Thread.sleep(2000);
         } 
